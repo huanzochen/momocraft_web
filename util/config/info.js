79643f910a02624064f5c63b63ceb0d110f8aca0
tommy.huang <tommy.huang@wavenet.com.tw>
2020-10-18 22:42:18 +0800

    新增 環境變數 .env 化

--- util/config/info.js
+++ util/config/info.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 exports = module.exports = {
-    host: 'localhost',
-    user: 'api',
-    database: 'web',
-    password: 'CHG_challenge112'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD
 }
