d0da2415a2c36b4663b929342017a1e7fffc2461
tommy huang <tommy.huang@wavenet.com.tw>
2020-12-01 16:51:19 +0800

    update mysqldump

--- documents/DB_DUMP/Dump20201201.sql
+++ documents/DB_DUMP/Dump20201201.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `web` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `web`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: web
@@ -160,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-01 12:30:45
+-- Dump completed on 2020-12-01 16:50:50
